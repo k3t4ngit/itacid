8a87514fe558ffb598f0d56aafd0cfb33b1db14e
k10 <k10@k10-PC>
2013-05-19 12:21:18 -0700

    ADDED: Drop animation

---
+++ _assets/js/site.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#loading').delay(400).fadeOut(1000);
+  $('#navigation').show().delay(600).animate({top: '0px'}, 500);
+  $('#footer').show().delay(600).animate({bottom: '0px'}, 500);
+//  $('#container').show().delay(1000).animate({left: '0px'}, 500);
+  $('#container').delay(1000).fadeIn(1000);
+});
+$('#itlogo').hover(function() {
+  $(this).addClass('rotateAnti');
+  $(this).attr('style',"margin-left:450px; margin-top:-40px;");
+  $('#maindrop').fadeIn(500).delay(00).animate({'margin-top': '100px'}, 1000);
+});
